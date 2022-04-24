e6aaba2b2ea29f26711c7dacb8973a5b39fdc8a4
dejvokep <davidcubesvkdev@gmail.com>
2022-04-24 10:45:39 +0200

    Background fix.

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -2,6 +2,7 @@ import {Fragment} from "react";
 import Header from "./header";
 import background from "../../public/background.jpg"
 import Head from "next/head";
+import styles from "../../styles/Background.module.css"
 
 export default function Layout(props) {
     return <Fragment>
@@ -10,18 +11,7 @@ export default function Layout(props) {
             <link rel="shortcut icon" href="/favicon.png" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
-        <div style={{
-            position: "fixed",
-            top: 0,
-            left: 0,
-            zIndex: -1,
-            height: "100vh",
-            width: "100%",
-            backgroundImage: `url(${background.src})`,
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-            backgroundAttachment: "center"
-        }}></div>
+        <div style={{backgroundImage: `url(${background.src})`}} className={styles.background} />
         <Header />
         {props.children}
     </Fragment>
